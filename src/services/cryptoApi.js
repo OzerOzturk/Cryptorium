bcc3d972361245af4197acc4ac0bc5917a03adb1
OzerOzturk <ozerozturk1636@gmail.com>
2021-12-30 23:59:57 +0300

    cryptocurrencies added with filtred option.

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,20 +1,20 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
-    
+   
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
 
-const createRequest = (url) => ({url, headers:cryptoApiHeaders})
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
 
 export const cryptoApi = createApi({
     reducerPath:'cryptoApi',
     baseQuery: fetchBaseQuery({baseUrl}),
     endpoints:(builder) => ({
-        getCryptos:builder.query({
-            query:() => createRequest('/coins')
+        getCryptos: builder.query({
+            query:(count) => createRequest(`/coins?limit=${count}`)
         })
     })
 });
